9373a752bd4b87b5d2cdc611ce98686692a37b87
Natacha290381 <natachacleon@gmail.com>
2025-07-24 14:53:28 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Kanban from './Kanban';
+
+export default function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>KANBAN - Service Achat</h1>
+      <Kanban />
+    </div>
+  );
+}
